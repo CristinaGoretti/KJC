9add50f09f666ebae27e602964afcfb7a067e000
kerenbagnoud <keren.bagnoud@heig-vd.ch>
2018-02-28 08:43:06 +0100

    DOCUMENTSTION

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,16 @@ router.get('/', function(req, res, next) {
 });*/
 
 /* GET users listing. */
+/**
+ * @api {get} /users/:id Request a user's information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Unique identifier of the user
+ *
+ * @apiSuccess {String} firstName First name of the user
+ * @apiSuccess {String} lastName  Last name of the user
+ */
 router.get('/', function(req, res, next) {
     User.find().sort('firstname').exec(function(err, users) {
         if (err) {
