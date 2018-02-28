cf63eda0fb1bba4eb4b80c86e293d504e15d5e85
kerenbagnoud <keren.bagnoud@heig-vd.ch>
2018-02-28 10:45:04 +0100

    users.js test

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+function loadUserFromParams(req, res, next) {
+    Person.findById(req.params.id).exec(function(err, user) {
+        if (err) {
+            return next(err);
+        } else if (!user) {
+            return res.status(404).send('No person found with ID ' + req.params.id);
+        }
+        req.user = user;
+        next();
+    });
+}
+
+function UpdatePerson(req, res, next) {
+    req.user.firstname = req.body.firstname;
+
+
+    req.user.save(function(err, updatedPerson) {
+        if (err) { return next(err); }
+        res.send(updatedPerson);
+    });
+}
+
+function RetrievePerson(req, res, next) {
+    // Update req.person here...
+    req.user.save(function(err, updatedPerson) {
+        if (err) { return next(err); }
+        res.send(updatedPerson);
+    });
+}
+
 /* POST new user */
 router.post('/', function(req, res, next) {
     // Create a new document from the JSON in the request body
@@ -40,5 +70,8 @@ router.get('/', function(req, res, next) {
         res.send(users);
     });
 });
+
+router.patch('/:id', loadUserFromParams, UpdatePerson);
+router.get('/:id', loadUserFromParams, RetrievePerson);
 
 module.exports = router;
