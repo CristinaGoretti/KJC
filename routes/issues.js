979c7b26b4c897d153ad932e81f8b36eb7a31454
Joséphine Chabin <josephine.chabin@gmail.com>
2018-03-08 08:36:20 +0100

    pagination

--- routes/issues.js
+++ routes/issues.js
@@ -165,14 +165,50 @@ router.post('/', function(req, res, next) {
  * @apiSuccess {String} firstName First name of the user
  * @apiSuccess {String} lastName  Last name of the user
  */
-/*router.get('/', function(req, res, next) {
-    Issue.find().sort('name').exec(function(err, issues) {
+
+router.get('/', function(req, res, next) {
+
+    Issue.find().count(function(err, total) {
+
         if (err) {
             return next(err);
         }
-        res.send(issues);
+
+        console.log(total);
+
+        let query = Issue.find();
+        // Parse the "page" param (default to 1 if invalid)
+        let page = parseInt(req.query.page, 10);
+        if (isNaN(page) || page < 1) {
+            page = 1;
+            console.log("Moins d1 page");
+        }
+        // Parse the "pageSize" param (default to 100 if invalid)
+        let pageSize = parseInt(req.query.pageSize, 10);
+        if (isNaN(pageSize) || pageSize < 0 || pageSize > 100) {
+            pageSize = 15;
+            console.log("plusieurs pages");
+        }
+        // Apply skip and limit to select the correct page of elements
+        query = query.skip((page - 1) * pageSize).limit(pageSize);
+
+        query.sort('firstname').exec(function(err, users) {
+            if (err) {
+                return next(err);
+            }
+
+            res.send({
+                page: page,
+                pageSize: pageSize,
+                total: total,
+                data: users
+            });
+        });
+
     });
-});*/
+
+
+});
 
 
 
